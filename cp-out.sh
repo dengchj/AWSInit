c8f9a407b3c8f463d09ce408fe8d76157578e60d
Chenjiong Deng <dengchenjiong@umcloud>
2018-11-29 11:44:08 +0800

    fix: make a classification

--- cp-out.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-nohup cp ./t1 /mnt/test/tt1 1>/dist/txtout1 &
-nohup cp ./t2 /mnt/test/tt2 1>/dist/txtout2 &
-nohup cp ./t3 /mnt/test/tt3 1>/dist/txtout3 &
-nohup cp ./t4 /mnt/test/tt4 1>/dist/txtout4 &
--- make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++11 fsync.cc -lrgw -o t-fsync
